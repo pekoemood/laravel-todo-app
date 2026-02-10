b74565ca5abb9368a82ee30edd63fdc65ea8f99b
Ryotarou Shiozawa <pekoemood@gmail.com>
2026-02-10 22:46:23 +0900

    log-2026-2-10

--- app/Notifications/TodoDueNotification.php
+++ app/Notifications/TodoDueNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TodoDueNotification extends Notification
+class TodoDueNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
